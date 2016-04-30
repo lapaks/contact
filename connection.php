3b6a93d393cd1735f10dac20710f09690558ec0a
lapaks <deepak.maharjan@outlook.com>
2016-04-30 11:36:50 -0400

    connection.php edited

--- connection.php
+++ connection.php
@@ -1,4 +1,6 @@
 <?php 
+
+/*
 $host="localhost"; // Host name 
 $username="pattake_pattake"; // Mysql username 
 $password="jhg(*&LKHJG#2"; // Mysql password 
@@ -7,16 +9,17 @@ $tbl_name="phone_book"; // Table name
 $tbl2="person_hist"; // Table name 
 $tbl3="grp_name"; // Table name 
 $tbl_usr="users"; // Table name 
-/*
+*/
+
 $host="localhost"; // Host name 
-$username="lapaks"; // Mysql username 
-$password="pattake"; // Mysql password 
-$db_name="lapaks"; // Database name 
+$username="root"; // Mysql username 
+$password=""; // Mysql password 
+$db_name="phone_book"; // Database name 
 $tbl_name="phone_book"; // Table name 
 $tbl2="person_hist"; // Table name 
 $tbl3="grp_name"; // Table name 
 $tbl_usr="users"; // Table name 
-*/
+
 
 // Connect to server and select databse.
 $con=mysqli_connect("$host", "$username", "$password", "$db_name")or die("cannot connect"); 
